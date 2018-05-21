c66d027602d2e08aa3fa0fe90a4392a878b35f6d
nusratarasg <nriaz2@my.centennialcollege.ca>
2018-05-21 15:58:27 -0400

    app.css updated

--- Scripts/app.js
+++ Scripts/app.js
@@ -3,12 +3,22 @@
 // IIFE - Immediately Invoked Function Expression
 // anonymous self executing function
 (function(){
-
+     "use strict"
     function Start() {
         // local variable
-        var title = document.title;
+        let title = document.title;
+        switch(title) {
+            case "COMP125 - Home":
+            break;
+            case "COMP125 - About":
+            break; 
+        }
+        let ABOUTBUTTON = document.getElementById("ABOUTBUTTON")
 
+        ABOUTBUTTON.addEventListener("click", function(){
 
+            Console.log("About Button was clicked")
+        });
         console.log("App Started!");
         console.log("----------------------------");
         console.log("Title: " + title);
